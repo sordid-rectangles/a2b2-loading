bb586a1bd5cec3c58dc12a9e6098ec73bf876a65
sordid-rectangles <98424209+sordid-rectangles@users.noreply.github.com>
2022-02-16 18:06:01 -0500

    init

---
+++ src/App.svelte
@@ -0,0 +1,22 @@
+<script>
+import Loading from "./Loading.svelte";
+</script>
+
+<main>
+	<Loading/>
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
